bbf03335f6f186dde4536ad842d32a4f50d23025
IdrissZ <idrisszuberu@gmail.com>
2018-01-19 17:00:15 +0000

    Initial Commit

---
+++ app/src/main/java/com/example/tradez0004/MyApp.java
@@ -0,0 +1,16 @@
+package com.example.tradez0004;
+
+import android.app.Application;
+import android.os.SystemClock;
+
+/**
+ * Created by IdrissZ on 06-Jan-18.
+ */
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        SystemClock.sleep(2000);
+    }
+}
